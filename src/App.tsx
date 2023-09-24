e92a70cbf030db814cfe9e1441aeb9312dcd2483
huchanghao <2955568032@qq.com>
2023-09-24 19:31:40 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { createSignal } from 'solid-js'
+
+const App = () => {
+  const [count,setCount] = createSignal(0)
+  return (
+    <button onClick={()=>setCount((pre)=>pre+1)}>
+      {count()}
+    </button>
+  )
+}
+
+export default App
